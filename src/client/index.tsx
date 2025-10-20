f3108ca754d724e88ce2fcd4dc4c06e7cd4df067
EdU1924 <eduardo21789@gmail.com>
2025-10-20 16:38:05 -0500

    ingreso api publica
    wa

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const http: AxiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL ?? "http://127.0.0.1:8000/api/",
+  baseURL: process.env.REACT_APP_API_BASE_URL ?? "https://apis-capstone.up.railway.app/api/",
   timeout: 300000,
   headers: {
     "Content-Type": "application/json",
